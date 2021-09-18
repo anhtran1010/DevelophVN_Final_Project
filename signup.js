705c585ea6dc67ed348bdd6f2e73cac290a883c0
chikien07012006 <87610465+chikien07012006@users.noreply.github.com>
2021-09-18 22:14:31 +0700

    Add files via upload

---
+++ signup.js
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+
+<title>Sign Up</title>
+<head>
+    <link rel="stylesheet" type="text/css" href="signup.css">
+
+</head>
+<body>
+<div class="wr">
+<form>
+    <div class="tt">Sign Up</div>
+    <br></br>
+    <div class="#">
+        <div>
+            <b>Email</b>
+        </div>
+        <input type="text" placeholder="Email" required>
+    </div>
+    <br></br>
+    <div class="#">
+        <div>
+            <b>Phone number</b>
+        </div>
+        <input type="text" placeholder="Phone number" required>
+    </div>
+    <br></br>
+    <div class="#">
+        <div>
+            <b>User name</b>
+        </div>
+        <input type="text" placeholder="User name" required>
+    </div>
+    <br></br>
+    <div class="#">
+        <div>
+            <b>Password</b>
+        </div>
+        <input type="text" placeholder="Password" required>
+    </div>
+    <br></br>
+    <div class="#">
+        <div>
+            <b>Current Level of Study</b>
+        </div>
+        <input type="text" placeholder="Current Level of Study" required>
+    </div>
+    <br></br>
+        <b>
+            <input type="checkbox"> I'm not a robot
+        </b>
+    <br></br>
+    <button >Submit</button>
+</form>
+</div>
+</body>
+</html>
+
+
