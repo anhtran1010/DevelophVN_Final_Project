055d19f75c61a2a26d501b8730f54ca01f030ccb
Giabao <luugiabaoluu24@gmail.com>
2021-10-31 23:20:58 +0700

    database updated

--- src/class.js
+++ src/class.js
@@ -1,9 +1,11 @@
-class College {
-    constructor(name, location, description, tuition, entrance_score, ) {
+let Colleges = class {
+    constructor(name, location, description, tuition, entrance_score, reviews) {
         this.name = name;
         this.location = location;
         this.description = description;
         this.tuition = tuition;
         this.entrance_score = entrance_score;
+        this.reviews = reviews;
     }
 }
+
